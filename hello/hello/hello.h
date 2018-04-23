c0dfb474e5e2b5b089293c9eb1dae71288b47f06
maweefeng <maweefeng@gmail.com>
2018-04-23 10:53:41 +0800

    da

---
+++ hello/hello/hello.h
@@ -0,0 +1,13 @@
+//
+//  hello.h
+//  hello
+//
+//  Created by Weefeng Ma on 2018/4/20.
+//  Copyright © 2018年 maweefeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface hello : NSObject
++(void)sayHi;
+@end
---
+++ hello/hello/hello.m
@@ -0,0 +1,15 @@
+//
+//  hello.m
+//  hello
+//
+//  Created by Weefeng Ma on 2018/4/20.
+//  Copyright © 2018年 maweefeng. All rights reserved.
+//
+
+#import "hello.h"
+
+@implementation hello
++(void)sayHi{
+    NSLog(@"hi");
+}
+@end
